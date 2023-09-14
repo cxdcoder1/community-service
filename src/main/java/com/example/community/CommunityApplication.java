5a06c225abd50697fdab326284a55f7a43257c28
whx <1060453316@qq.com>
2023-09-14 19:33:44 +0800

    登录

--- src/main/java/com/example/community/CommunityApplication.java
+++ src/main/java/com/example/community/CommunityApplication.java
@@ -3,9 +3,11 @@ package com.example.community;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 @MapperScan("com.example.community.dao")
+@CrossOrigin
 public class CommunityApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/example/community/controller/SysUserController.java
+++ src/main/java/com/example/community/controller/SysUserController.java
@@ -2,17 +2,25 @@ package com.example.community.controller;
 
 
 
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.api.ApiController;
 import com.baomidou.mybatisplus.extension.api.R;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.example.community.constant.SystemConstant;
+import com.example.community.entity.JwtUtil;
 import com.example.community.entity.SysUser;
 import com.example.community.service.SysUserService;
 import org.springframework.web.bind.annotation.*;
+import com.alibaba.fastjson.JSON;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.io.Serializable;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 用户信息表(SysUser)表控制层
@@ -57,6 +65,43 @@ public class SysUserController extends ApiController {
     @GetMapping("{id}")
     public R selectOne(@PathVariable Serializable id) {
         return success(this.sysUserService.getById(id));
+    }
+
+    /**
+     * 登录
+     * @param user
+     * @return
+     */
+    @RequestMapping("login")
+    public Map<String,Object> login(SysUser user, HttpServletRequest request) throws Exception {
+        Map<String,Object> result = new HashMap<>();
+        //校验用户名和密码
+        QueryWrapper queryWrapper = new QueryWrapper<SysUser>();
+        queryWrapper.eq("phonenumber",user.getPhonenumber());
+        //根据电话查询用户z
+        SysUser user1 = sysUserService.getOne(queryWrapper);
+        if (user1 != null){
+            if (user1.getPassword().equals(user.getPassword())){
+                //将用户信息存储到Session  //登入成功
+                HttpSession session = request. getSession();
+                session. setAttribute("userInfo", user1);  //1.用于拦截器的判断  2.界面显示用户信息
+                result. put("Id", user1.getUserId());
+                //把token返回给客户端-->客户端保存至localStorage-->客户端每次请求附带localStorage参数
+                //SystemConstant.JWT_TTL：token有效时间
+                String JWT = JwtUtil.createJWT("1", JSON.toJSONString(user1), SystemConstant.JWT_TTL);
+//                log.info(JWT);
+                result.put("JWT",JWT);
+                return result;
+            }
+            //登录失败 ，密码错误
+            result. put("success", false);
+            result. put("msg", "密码错误!");
+            return result;
+        }
+        //登录失败，用户名错误或不存在
+        result. put("success", false);
+        result. put("msg", "用户名错误或不存在!");
+        return result;
     }
 
     /**
