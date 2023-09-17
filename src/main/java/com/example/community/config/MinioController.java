c7be198056c0a6a6c324dee641111581c1756ed7
cxd <2787450838@qq.com>
2023-09-17 20:14:21 +0800

    第二天项目删除完成

---
+++ src/main/java/com/example/community/config/MinioController.java
@@ -0,0 +1,36 @@
+package com.example.community.config;
+
+import com.example.community.utils.MinioUtilS;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+
+import java.util.List;
+
+@RestController
+@Slf4j
+@CrossOrigin
+public class MinioController {
+    @Autowired
+    private MinioUtilS minioUtilS;
+    @Value("${minio.endpoint}")
+    private String address;
+    @Value("${minio.bucketName}")
+    private String bucketName;
+
+    @PostMapping("/upload")
+    public Object upload(MultipartFile file) {
+       
+        List<String> upload = minioUtilS.upload(new MultipartFile[]{file});
+
+
+
+        return address+"/"+bucketName+"/"+upload.get(0);
+    }
+
+}
