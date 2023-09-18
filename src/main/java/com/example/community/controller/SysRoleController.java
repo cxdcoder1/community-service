ee38cac575de06732d1c7ccedd99115aed81cf2e
cxd <2787450838@qq.com>
2023-09-18 20:03:06 +0800

    Revert "第二天项目完成版"
    This reverts commit 05cdff75518e59f201c06a9ec35c5b9ec617739a.
    
    # Conflicts:
    #	src/main/java/com/example/community/controller/SysRoleController.java

--- src/main/java/com/example/community/controller/SysRoleController.java
+++ src/main/java/com/example/community/controller/SysRoleController.java
@@ -1,16 +1,19 @@
 package com.example.community.controller;
 
 
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.api.ApiController;
 import com.baomidou.mybatisplus.extension.api.R;
-import com.example.community.entity.SysMenu;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.example.community.entity.SysRole;
 import com.example.community.service.SysRoleService;
-import com.example.community.utils.MenuTree;
-import org.springframework.validation.annotation.Validated;
+import org.apache.ibatis.session.RowBounds;
 import org.springframework.web.bind.annotation.*;
 
+
 import javax.annotation.Resource;
+import java.io.Serializable;
 import java.util.List;
 
 /**
@@ -27,43 +30,70 @@ public class SysRoleController extends ApiController {
      * 服务对象
      */
     @Resource
-    private SysRoleService roleService;
+    private SysRoleService sysRoleService;
 
-    /**
-     * 修改保存角色
-     */
-    @PutMapping("edit")
-    public String edit(@Validated @RequestBody SysRole role) {
-        int i = roleService.updateRole(role);
-        if (i!=0){
-            return "修改成功";
-        }else {
-            return "修改失败";
-        }
+
+
+    @GetMapping("/list")
+    public List<SysRole> list(SysRole role) {
+        List<SysRole> list = sysRoleService.selectRoleList(role);
+        return list;
     }
 
     /**
-     * 修改保存数据权限
+     * 分页查询所有数据
+     *
+     * @param page 分页对象
+     * @param sysRole 查询实体
+     * @return 所有数据
      */
-//    @PutMapping("/dataScope")
-//    public String dataScope(@RequestBody SysRole role) {
-//        roleService.
-//    }
+    @GetMapping
+    public R selectAll(Page<SysRole> page, SysRole sysRole) {
+        return success(this.sysRoleService.page(page, new QueryWrapper<>(sysRole)));
+    }
 
-    @RequestMapping("getRoleMenuTreeselect")
-    public R getRoleMenuTreeselect(int roleId){
-        List<SysMenu> menus = roleService.getRoleMenuTreeselect(roleId);
-        List<SysMenu> menuList = new MenuTree(menus).builTree();
+    /**
+     * 通过主键查询单条数据
+     *
+     * @param id 主键
+     * @return 单条数据
+     */
+    @GetMapping("{id}")
+    public R selectOne(@PathVariable Serializable id) {
+        return success(this.sysRoleService.getById(id));
+    }
 
-        if (menuList != null && !menuList.isEmpty()) {
-            System.out.println(menuList);
-            return R.ok(menuList);
-        }
+    /**
+     * 新增数据
+     *
+     * @param sysRole 实体对象
+     * @return 新增结果
+     */
+    @PostMapping
+    public R insert(@RequestBody SysRole sysRole) {
+        return success(this.sysRoleService.save(sysRole));
+    }
 
-        System.err.println(R.ok(menus));
+    /**
+     * 修改数据
+     *
+     * @param sysRole 实体对象
+     * @return 修改结果
+     */
+    @PutMapping
+    public R update(@RequestBody SysRole sysRole) {
+        return success(this.sysRoleService.updateById(sysRole));
+    }
 
-        return R.ok(menus);
-
+    /**
+     * 删除数据
+     *
+     * @param idList 主键结合
+     * @return 删除结果
+     */
+    @DeleteMapping
+    public R delete(@RequestParam("idList") List<Long> idList) {
+        return success(this.sysRoleService.removeByIds(idList));
     }
 }
 
--- src/main/java/com/example/community/service/SysRoleService.java
+++ src/main/java/com/example/community/service/SysRoleService.java
@@ -1,7 +1,6 @@
 package com.example.community.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
-import com.example.community.entity.SysMenu;
 import com.example.community.entity.SysRole;
 
 import java.util.List;
@@ -13,47 +12,6 @@ import java.util.List;
  * @since 2023-09-14 09:53:02
  */
 public interface SysRoleService extends IService<SysRole> {
-    /**
-     * 通过角色ID查询角色
-     *
-     * @param roleId 角色ID
-     * @return 角色对象信息
-     */
-    public SysRole selectRoleById(Long roleId);
-
-    /**
-     * 校验角色名称是否唯一
-     *
-     * @param role 角色信息
-     * @return 结果
-     */
-    public String checkRoleNameUnique(SysRole role);
-
-    /**
-     * 校验角色权限是否唯一
-     *
-     * @param role 角色信息
-     * @return 结果
-     */
-    public String checkRoleKeyUnique(SysRole role);
-
-    /**
-     * 新增保存角色信息
-     *
-     * @param role 角色信息
-     * @return 结果
-     */
-    public int insertRole(SysRole role);
-
-    /**
-     * 修改保存角色信息
-     *
-     * @param role 角色信息
-     * @return 结果
-     */
-    public int updateRole(SysRole role);
-
-    public List<SysMenu> getRoleMenuTreeselect(int roleId);
 
     public List<SysRole> selectRoleList(SysRole role);
 
